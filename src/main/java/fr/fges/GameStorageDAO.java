a3f9c90046e267a6c161a2504057b8eb704df3ef
Issam <issamito573@gmail.com>
2026-02-20 17:35:10 +0100

    Ajout des interfaces contractuelles

---
+++ src/main/java/fr/fges/GameStorageDAO.java
@@ -0,0 +1,23 @@
+package fr.fges;
+
+import java.util.List;
+
+/**
+ * Interface DAO pour la persistence des jeux.
+ * Contrat que chaque implémentation (JSON, CSV) doit respecter.
+ */
+public interface GameStorageDAO {
+    /**
+     * Charge les jeux depuis le fichier de stockage.
+     * @param filename Chemin du fichier
+     * @return Liste des jeux chargés
+     */
+    List<BoardGame> load(String filename);
+
+    /**
+     * Sauvegarde les jeux dans le fichier de stockage.
+     * @param filename Chemin du fichier
+     * @param games Liste des jeux à sauvegarder
+     */
+    void save(String filename, List<BoardGame> games);
+}
