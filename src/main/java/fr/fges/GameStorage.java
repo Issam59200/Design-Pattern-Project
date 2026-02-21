0b70c66ed3fb9cd1ab7c7ed4617abde817c5ef23
Enzo <enzoniels88@gmail.com>
2026-02-21 23:08:21 +0000

    Suppression de l'interface GameStorageDAO car GameStorage fait la même chose

--- src/main/java/fr/fges/GameStorage.java
+++ src/main/java/fr/fges/GameStorage.java
@@ -2,7 +2,22 @@ package fr.fges;
 
 import java.util.List;
 
+/**
+ * Interface DAO pour la persistence des jeux.
+ * Contrat que chaque implémentation (JSON, CSV) doit respecter.
+ */
 public interface  GameStorage {
+    /**
+     * Charge les jeux depuis le fichier de stockage.
+     * @param filename Chemin du fichier
+     * @return Liste des jeux chargés
+     */
     List<BoardGame> load(String filename);
+
+    /**
+     * Sauvegarde les jeux dans le fichier de stockage.
+     * @param filename Chemin du fichier
+     * @param games Liste des jeux à sauvegarder
+     */
     void save(String filename, List<BoardGame> games);
 }
